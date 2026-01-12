156427867fedd69c2e0a015149e16ff1e65234a5
Pau <pau@mail.com>
2026-01-12 19:18:34 +0100

    cambio cuatro

--- TablaDel12.java
+++ TablaDel12.java
@@ -7,6 +7,6 @@ public class TablaDel12
             System.out.println("12 * "+ i + " = " + 12 * i + "\n");
         }
 
-        System.out.println("> Se ha imprimido por pantalla la tabla del 12");
+        System.out.println("> Desarrollado para la aplicación Maths.");
     }
 }
